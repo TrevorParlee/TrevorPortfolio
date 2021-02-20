d742555c0da3f5016ab8493ff3146a185dcadeec
Trevor Parlee <trevorparlee@gmail.com>
2021-02-20 00:34:41 -0700

    portfolio done-ish

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import image from "../5pK3M4T.jpg";
+
+export default function Home() {
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Picture of Trevor smilling"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative text-center flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hello. My name is Trevor Parlee and this page is all about me!
+        </h1>
+      </section>
+    </main>
+  );
+}
