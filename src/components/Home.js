92b5d765a849872ddaddc9e492f497f0139fa593
Trevor Parlee <trevorparlee@gmail.com>
2021-02-28 12:17:40 -0700

    fixed spacing for post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import image from '../5pK3M4T.jpg';
+import image from '../DSCF0172.jpg';
 
 export default function Home() {
   return (
@@ -9,9 +9,9 @@ export default function Home() {
         alt='Picture of Trevor smiling'
         className='absolute object-cover w-full h-full'
       />
-      <section className='relative text-center flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
-        <h1 className='text-6xl text-white font-bold cursive leading-none lg:leading-snug home-name'>
-          Hello. My name is Trevor Parlee and this page is all about me!
+      <section className='relative text-center flex justify-center min-h-screen pt-12 lg:pt-44 px-8'>
+        <h1 className='text-6xl text-blue-600 font-bold cursive leading-none lg:leading-snug home-name'>
+          Welcome to Trevor Parlee's portfolio page!
         </h1>
       </section>
     </main>
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import sanityClient from "../client.js";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import sanityClient from '../client.js';
 
 export default function Post() {
   const [postData, setPost] = useState(null);
@@ -26,30 +26,30 @@ export default function Post() {
   }, []);
 
   return (
-    <main className="bg-blue-100 min-h-screen p-12">
-      <section className="container mx-auto">
-        <h1 className="text-5xl flex justify-center cursive">
+    <main className='bg-blue-100 min-h-screen p-12'>
+      <section className='container mx-auto'>
+        <h1 className='text-5xl flex justify-center cursive'>
           Blog Posts Page
         </h1>
-        <h2 className="text-lg text-grey-600 flex justify-center mb-12">
+        <h2 className='text-lg text-grey-600 flex justify-center mb-12'>
           Welcome to my page of blog posts
         </h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-20'>
           {postData &&
             postData.map((post, index) => (
               <article>
-                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                <Link to={'/post/' + post.slug.current} key={post.slug.current}>
                   <span
-                    className="block h-64 relative rounded shadow leading-snug bg-gray-800 border-8 border-white"
+                    className='block h-64 relative rounded shadow leading-snug bg-gray-800 border-8 border-white'
                     key={index}
                   >
                     <img
                       src={post.mainImage.asset.url}
                       alt={post.mainImage.alt}
-                      className="block relative h-full flex justify-end items-end "
+                      className='block relative h-full flex justify-end items-center '
                     />
                     <span>
-                      <h3 className="text-white text-xl font-blog px-3 py-4 bg-blue-700 text-white bg-opacity-75 border-8 border-white rounded">
+                      <h3 className='text-white text-xl font-blog px-3 py-4 bg-blue-700 text-white bg-opacity-75 border-8 border-white rounded pb-100'>
                         {post.title}
                       </h3>
                     </span>
