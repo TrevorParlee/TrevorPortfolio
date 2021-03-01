f3c45dc7e77484760251286196c7c63e46310db1
Trevor Parlee <trevorparlee@gmail.com>
2021-02-28 19:27:06 -0700

    location not company

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import sanityClient from "../client.js";
+import React, { useEffect, useState } from 'react';
+import sanityClient from '../client.js';
 
 export default function Project() {
   const [projectData, setProjectData] = useState(null);
@@ -22,50 +22,50 @@ export default function Project() {
   }, []);
 
   return (
-    <main className=" bg-blue-300 min-h-screen p-12">
-      <section className="container mx-auto ">
-        <h1 className="text-5xl flex justify-center cursive">My Projects </h1>
-        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+    <main className=' bg-blue-300 min-h-screen p-12'>
+      <section className='container mx-auto '>
+        <h1 className='text-5xl flex justify-center cursive'>My Projects </h1>
+        <h2 className='text-lg text-gray-600 flex justify-center mb-12'>
           Welcome to my projects page!!!
         </h2>
-        <section className="grid grid-cols-2 gap-8 ">
+        <section className='grid grid-cols-2 gap-8 '>
           {projectData &&
             projectData.map((project, index) => (
-              <article className="relative rounded-lg shadow-xl bg-white p-16 border-8 border-gray-800">
-                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+              <article className='relative rounded-lg shadow-xl bg-white p-16 border-8 border-gray-800'>
+                <h3 className='text-gray-800 text-3xl font-bold mb-2 hover:text-red-700'>
                   <a
                     href={project.link}
                     alt={project.title}
-                    target="_blank"
-                    rel="noopener noreferrer"
+                    target='_blank'
+                    rel='noopener noreferrer'
                   >
                     {project.title}
                   </a>
                 </h3>
-                <div className="text-gray-500 text-xs space-x-4">
+                <div className='text-gray-500 text-xs space-x-4'>
                   <span>
-                    <strong className="font-bold">Finished on</strong>:{" "}
+                    <strong className='font-bold'>Finished on</strong>:{' '}
                     {new Date(project.date).toLocaleDateString()}
                   </span>
                   <span>
-                    <strong className="font-bold">Company</strong>:{" "}
+                    <strong className='font-bold'>Location</strong>:{' '}
                     {project.place}
                   </span>
                   <span>
-                    <strong className="font-bold">Type</strong>:{" "}
+                    <strong className='font-bold'>Type</strong>:{' '}
                     {project.projectType}
                   </span>
-                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                  <p className='my-6 text-lg text-gray-700 leading-relaxed'>
                     {project.description}
                   </p>
                   <a
                     href={project.link}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="text-blue-700 font-bold hover:underline hover:text-red-400 text-xl"
+                    rel='noopener noreferrer'
+                    target='_blank'
+                    className='text-blue-700 font-bold hover:underline hover:text-red-400 text-xl'
                   >
-                    View The Project{" "}
-                    <span role="img" aria-label="right pointer">
+                    View The Project{' '}
+                    <span role='img' aria-label='right pointer'>
                       👉
                     </span>
                   </a>
